099aee070b868d6c1f4c53e247a19ec89c07d5c8
yogasaichunduri-04 <yogasaichunduri@gmail.com>
2025-08-08 10:41:36 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
           <h1 className="text-5xl md:text-7xl font-bold mb-6 tracking-tight">
             Hi, I'm{" "}
             <span className="gradient-text">
-              Raheel
+              Chunduri Yogasai
             </span>
           </h1>
           
